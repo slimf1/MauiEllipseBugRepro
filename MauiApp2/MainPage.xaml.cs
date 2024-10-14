ffd392873277de7c6d4aa9792be41ca42e88182b
slimf1 <slimane.fakani.dev@gmail.com>
2024-10-14 23:29:32 +0200

    Initial commit

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_OnClicked(object? sender, EventArgs e)
+    {
+        await Ellipse.ScaleTo(0.5, 1000);
+    }
+}
